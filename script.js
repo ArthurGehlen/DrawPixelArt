93734d48cdb6c4e8b43b48654ce72955653de091
Arthur Gehlen <1arthurgehlen@gmail.com>
2024-11-21 13:14:38 -0300

    Commit Inicial, está funcionando, mas tem que fazer uma interface.

---
+++ script.js
@@ -0,0 +1,63 @@
+let rows = 10;
+let columns = 10;
+
+window.onload = function () {
+    criar_painel();
+    criar_paleta();
+};
+
+function btn_click(ev) {
+    // ev.srcElement.classList.toggle('color');
+    let color = document.documentElement.style.getPropertyValue('--current_color');
+    ev.srcElement.style.backgroundColor = color;
+}
+
+function criar_botao(texto) {
+    let botao = document.createElement("button");
+    botao.innerHTML = texto;
+    botao.addEventListener('click', btn_click);
+
+    let painel = document.getElementById("painel");
+    painel.appendChild(botao);
+}
+
+function criar_painel()  {
+    let num_elements = rows * columns;
+
+    for (let i = 0; i <= num_elements - 1; i++)  {
+        criar_botao(i + 1);
+    }
+}
+
+function aplicar_configuracoes()  {
+    let linhas = document.getElementById("linhas").value;
+    let colunas = document.getElementById("colunas").value;
+
+    if (linhas > 0 && columns > 0)  {
+        document.getElementById("painel").innerHTML = "";
+        rows = linhas;
+        columns = colunas;
+        document.documentElement.style.setProperty('--row', linhas);
+        document.documentElement.style.setProperty('--col', colunas);
+        criar_painel();
+    }
+}
+
+function criar_paleta() {
+    let paleta = document.getElementById("paleta");
+    let cores = ['#25CCF7', '#EAB543', '#000', '#F97F51', '#FFF', '#82589F', '#FC427B', '#55E6C1'];
+    let num_cores = cores.length;
+
+    for (let i = 0; i <= num_cores - 1; i++) {
+        let botao = document.createElement("button");
+        botao.style.backgroundColor = cores[i];
+        botao.addEventListener('click', click_paleta);
+        paleta.appendChild(botao);
+    }
+}
+
+function click_paleta(ev) {
+    let color = ev.srcElement.style.backgroundColor;
+    console.log(color);
+    document.documentElement.style.setProperty('--current_color', color);
+}
